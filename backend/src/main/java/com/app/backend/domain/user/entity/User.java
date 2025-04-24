8e285c54ef09a837744ba883078931e78174376c
swjoon <jetkid1446@gmail.com>
2025-04-25 01:26:45 +0900

    feat : user entity 컬럼 추가

--- backend/src/main/java/com/app/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/app/backend/domain/user/entity/User.java
@@ -1,23 +1,70 @@
 package com.app.backend.domain.user.entity;
 
+import java.time.LocalDate;
+
 import com.app.backend.global.entity.BaseEntity;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Table(name = "tbl_users")
+@Entity
 @Getter
+@Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id", nullable = false, updatable = false)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id", nullable = false, updatable = false)
+	private Long id;
 
+	@Column(unique = true, nullable = false)
+	private String username;
 
+	@Column
+	private String password;
+
+	@Column(nullable = false, length = 20)
+	private String name;
+
+	@Column(unique = true, length = 30)
+	private String nickname;
+
+	@Column(unique = true, nullable = false)
+	private String email;
+
+	@Column(unique = true, nullable = false)
+	private String phone;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+
+	@Column(nullable = false)
+	private LocalDate birthDate;
+
+	@Builder.Default
+	@Enumerated(EnumType.STRING)
+	private SocialType socialType = SocialType.LOCAL;
+
+	private String providerId;
+
+	private String profileImageUrl;
+
+	@Builder.Default
+	@Enumerated(EnumType.STRING)
+	private Role role = Role.ROLE_USER;
 }
