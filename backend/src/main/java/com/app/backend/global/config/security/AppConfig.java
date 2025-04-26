135e2f507607b50b1d0222990bbeeab0cb81d199
swjoon <jetkid1446@gmail.com>
2025-04-26 13:16:21 +0900

    feat : 로그인 및 security 관련 설정

---
+++ backend/src/main/java/com/app/backend/global/config/security/AppConfig.java
@@ -0,0 +1,20 @@
+package com.app.backend.global.config.security;
+
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public Jackson2ObjectMapperBuilderCustomizer addJavaTimeModule() {
+		return builder -> {
+			builder.modules(new JavaTimeModule());
+			builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		};
+	}
+}
---
+++ backend/src/main/java/com/app/backend/global/config/security/config/CorsConfig.java
@@ -0,0 +1,38 @@
+package com.app.backend.global.config.security.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+	@Value("${url:\"http://localhost:3000\"}")
+	private String urlList;
+
+	@Bean
+	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		// 허용할 오리진 설정
+		configuration.setAllowedOrigins(Arrays.stream(urlList.split(",")).toList());
+		// 허용할 HTTP 메서드 설정
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+		// 자격 증명 허용 설정
+		configuration.setAllowCredentials(true);
+		// 허용할 헤더 설정
+		configuration.setAllowedHeaders(Collections.singletonList("*"));
+
+		configuration.setExposedHeaders(List.of("Authorization"));
+
+		// CORS 설정을 소스에 등록
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
