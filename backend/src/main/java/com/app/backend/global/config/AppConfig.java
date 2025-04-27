92717dbe73d664aaf00f9eb65dd1a1e6bbea1370
swjoon <jetkid1446@gmail.com>
2025-04-27 16:13:24 +0900

    Rename : AppConfig 파일 위치 변경

--- backend/src/main/java/com/app/backend/global/config/AppConfig.java
+++ backend/src/main/java/com/app/backend/global/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.app.backend.global.config.security;
+package com.app.backend.global.config;
 
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
