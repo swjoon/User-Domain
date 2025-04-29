a4f0e7f1b2665c292f1aecb5b2dbc3bc9076d90b
swjoon <jetkid1446@gmail.com>
2025-04-29 19:21:07 +0900

    test : passwordEncoder 수정

--- backend/src/test/java/com/app/backend/global/util/SpringbootTestUtil.java
+++ backend/src/test/java/com/app/backend/global/util/SpringbootTestUtil.java
@@ -2,20 +2,24 @@ package com.app.backend.global.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.app.backend.domain.user.repository.UserRepository;
 import com.app.backend.domain.user.service.UserService;
+import com.app.backend.global.config.TestOAuth2Config;
 
 import jakarta.persistence.EntityManager;
 
-@ActiveProfiles("test")
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestOAuth2Config.class)
 public abstract class SpringbootTestUtil {
 
 	@Autowired
-	protected BCryptPasswordEncoder bCryptPasswordEncoder;
+	protected PasswordEncoder passwordEncoder;
 
 	@Autowired
 	protected EntityManager em;
