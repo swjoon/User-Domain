006f2b3eac0bed37511e1d36d7c0e212e26dc450
swjoon <jetkid1446@gmail.com>
2025-05-20 17:35:16 +0900

    refactor : config 일부 수정

--- backend/src/main/java/com/app/backend/global/config/security/config/CorsConfig.java
+++ backend/src/main/java/com/app/backend/global/config/security/config/CorsConfig.java
@@ -1,10 +1,8 @@
 package com.app.backend.global.config.security.config;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,14 +11,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
-	@Value("${url:\"http://localhost:3000\"}")
-	private String urlList;
-
 	@Bean
 	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		// 허용할 오리진 설정
-		configuration.setAllowedOrigins(Arrays.stream(urlList.split(",")).toList());
+		configuration.setAllowedOrigins(List.of("http://localhost:3000"));
 		// 허용할 HTTP 메서드 설정
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 		// 자격 증명 허용 설정
