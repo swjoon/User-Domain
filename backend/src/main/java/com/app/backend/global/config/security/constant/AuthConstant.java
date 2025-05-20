ed12bcee5d8358ad5dad8bd943f236a11d998234
swjoon <jetkid1446@gmail.com>
2025-05-20 17:33:35 +0900

    feat : 상수 추가

--- backend/src/main/java/com/app/backend/global/config/security/constant/AuthConstant.java
+++ backend/src/main/java/com/app/backend/global/config/security/constant/AuthConstant.java
@@ -1,10 +1,15 @@
 package com.app.backend.global.config.security.constant;
 
 public abstract class AuthConstant {
+	public static final String UUID = "UUID";
+	public static final String PROVIDER = "PROVIDER";
 	public static final String GOOGLE = "GOOGLE";
 	public static final String NAVER = "NAVER";
 	public static final String KAKAO = "KAKAO";
-	public static final String REDIS_TOKEN_PREFIX = "auth:";
 	public static final String REFRESH_TOKEN = "refreshToken";
+	public static final String ACCESS_TOKEN = "AccessToken";
 	public static final String AUTHENTICATION = "Authorization";
+	public static final String ACCESS_TOKEN_PREFIX = "Bearer ";
+	public static final String REDIS_TOKEN_PREFIX = "auth:";
+	public static final String OAUTH2_LOGIN_UUID_PREFIX = "oauth2_login_uuid";
 }
